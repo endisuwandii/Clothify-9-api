5ded9e33250387a013a7c735cf7b42f0ddf48cfe
endisuwandi <endisuwandi64@gmail.com>
2025-10-15 15:04:16 +0700

    chore: add new data schema

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -8,4 +8,13 @@ export const dataProducts = [
     stock: 100,
     description: "Detail Produk : Bahan Combed, Kerah bulat,  Sablon, ",
   },
+  {
+    slug: "russ-tshirt-struggle-white",
+    name: "Kaos Russ Perjuangan Putih",
+    imageUrl:
+      "https://www.russ.co.id/cdn/shop/products/rug-1637904964782-0_1400x.jpg?v=1739797127",
+    price: 195000,
+    stock: 2,
+    description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
+  },
 ];
