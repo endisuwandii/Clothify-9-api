f0061710f073cc83fe1148f326149c92ac5eb7e4
endisuwandi <endisuwandi64@gmail.com>
2025-10-15 09:54:44 +0700

    chore: setup seed,products,index and schema file

---
+++ prisma/data/products.ts
@@ -0,0 +1,11 @@
+export const dataProducts = [
+  {
+    slug: "russ-tshirt-lufy-white",
+    name: "Russ Tshirt Lufy White",
+    imageUrl:
+      "https://www.russ.co.id/cdn/shop/products/rug-1642143060484-1_1400x.jpg?v=1739797025",
+    price: 195000,
+    stock: 100,
+    description: "Detail Produk : Bahan Combed, Kerah bulat,  Sablon, ",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,78 @@
-import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+import { Scalar } from "@scalar/hono-api-reference";
+
 import { db } from "./lib/db";
+import {
+  ProductSlugParamSchema,
+  ProductSchema,
+  ProductsSchema,
+} from "./modules/product/schema";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
-app.get("/", (c) => {
-  return c.text("Welcome User!");
+app.use(cors());
+
+app.openapi(
+  createRoute({
+    method: "get",
+    path: "/products",
+    responses: {
+      200: {
+        description: "Get all products",
+        content: { "application/json": { schema: ProductsSchema } },
+      },
+    },
+  }),
+  async (c) => {
+    const products = await db.product.findMany();
+
+    return c.json(products);
+  }
+);
+
+app.openapi(
+  createRoute({
+    method: "get",
+    path: "/products/{slug}",
+    request: { params: ProductSlugParamSchema },
+    responses: {
+      200: {
+        description: "Get one product by slug",
+        content: { "application/json": { schema: ProductSchema } },
+      },
+      404: {
+        description: "Product by slug not found",
+      },
+    },
+  }),
+  async (c) => {
+    const { slug } = c.req.valid("param");
+
+    const product = await db.product.findUnique({ where: { slug } });
+
+    if (!product) {
+      return c.notFound();
+    }
+
+    return c.json(product);
+  }
+);
+
+app.doc("/openapi.json", {
+  openapi: "3.0.0",
+  info: {
+    title: "Clohify API",
+    version: "1.0.0",
+  },
 });
 
-app.get("/products", async (c) => {
-  const products = await db.product.findMany();
-
-  return c.json(products);
-});
+app.get(
+  "/",
+  Scalar({
+    pageTitle: "Clohify API",
+    url: "/openapi.json",
+  })
+);
 
 export default app;
