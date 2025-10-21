0297e32b6e94923b22b70e2948db67438460feb9
endisuwandi <endisuwandi64@gmail.com>
2025-10-21 19:15:48 +0700

    chore: setup new data products

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -17,4 +17,41 @@ export const dataProducts = [
     stock: 2,
     description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
   },
+  {
+    slug: "russ-tshirt-collect-white",
+    name: "Kaos Russ Putih",
+    imageUrl:
+      "https://www.russ.co.id/cdn/shop/products/ginee_20220704110643782_5557375311_1400x.jpg?v=1739796843",
+    price: 104900,
+    stock: 2,
+    description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
+  },
+  {
+    slug: "russ-tshirt-movie-time-white",
+    name: "Russ Tshirt Film Waktu Putih",
+    imageUrl:
+      "https://www.russ.co.id/cdn/shop/products/rug-1637904140576-1_1000x.jpg?v=1739797108",
+    price: 104900,
+    stock: 12,
+    description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
+  },
+
+  {
+    slug: "russ-tshirt-straighfor-ss-black",
+    name: "Kaos Russ Straighfor SS Hitam",
+    imageUrl:
+      "https://www.russ.co.id/cdn/shop/products/BLACK_1000x.jpg?v=1739797254",
+    price: 104900,
+    stock: 13,
+    description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
+  },
+
+  // {
+  //   slug: "",
+  //   name: "",
+  //   imageUrl: "",
+  //   price: 0,
+  //   stock: 10,
+  //   description: "",
+  // },
 ];
