b42188ef0aae08f49e95e9e33f95554d3ba01e8a
endisuwandi <endisuwandi64@gmail.com>
2025-10-31 20:11:51 +0700

    feat: add login endpoint for user authentication with password verification

--- src/index.ts
+++ src/index.ts
@@ -159,6 +159,66 @@ app.openapi(
   }
 );
 
+app.openapi(
+  createRoute({
+    method: "post",
+    path: "/auth/login",
+    request: {
+      body: { content: { "application/json": { schema: LoginUserSchema } } },
+    },
+    responses: {
+      200: {
+        description: "Logged in to user",
+        content: { "text/plain": { schema: TokenSchema } },
+      },
+      400: {
+        description: "Failed to login user",
+      },
+      404: {
+        description: "User not found",
+      },
+    },
+  }),
+  async (c) => {
+    const body = c.req.valid("json");
+
+    try {
+      const user = await db.user.findUnique({
+        where: { email: body.email },
+        include: {
+          password: true,
+        },
+      });
+      if (!user) {
+        return c.notFound();
+      }
+
+      if (!user.password?.hash) {
+        return c.json({
+          message: "User has no password",
+        });
+      }
+
+      const isMatch = await Bun.password.verify(
+        body.password,
+        user.password.hash
+      );
+
+      if (!isMatch) {
+        return c.json({
+          message: "Password incorrect",
+        });
+      }
+
+      const token = await signToken(user.id);
+
+      return c.text(token);
+    } catch (error) {
+      return c.json({ message: "Email or password is incorrect" }, 400);
+    }
+  }
+);
+
 app.doc("/openapi.json", {
   openapi: "3.0.0",
   info: {
