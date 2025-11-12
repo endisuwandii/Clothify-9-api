c7624719c1e45c98d728fd9b821aa024c98552df
endisuwandi <endisuwandi64@gmail.com>
2025-11-12 14:35:26 +0700

    chore: refactor price

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -4,7 +4,7 @@ export const dataProducts = [
     name: "Russ Tshirt Lufy White",
     imageUrl:
       "https://www.russ.co.id/cdn/shop/products/rug-1642143060484-1_1400x.jpg?v=1739797025",
-    price: 195000,
+    price: 1950,
     stock: 100,
     description: "Detail Produk : Bahan Combed, Kerah bulat,  Sablon, ",
   },
@@ -13,7 +13,7 @@ export const dataProducts = [
     name: "Kaos Russ Perjuangan Putih",
     imageUrl:
       "https://www.russ.co.id/cdn/shop/products/rug-1637904964782-0_1400x.jpg?v=1739797127",
-    price: 195000,
+    price: 1950,
     stock: 2,
     description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
   },
@@ -22,7 +22,7 @@ export const dataProducts = [
     name: "Kaos Russ Putih",
     imageUrl:
       "https://www.russ.co.id/cdn/shop/products/ginee_20220704110643782_5557375311_1400x.jpg?v=1739796843",
-    price: 104900,
+    price: 1049,
     stock: 2,
     description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
   },
@@ -31,7 +31,7 @@ export const dataProducts = [
     name: "Russ Tshirt Film Waktu Putih",
     imageUrl:
       "https://www.russ.co.id/cdn/shop/products/rug-1637904140576-1_1000x.jpg?v=1739797108",
-    price: 104900,
+    price: 1049,
     stock: 12,
     description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
   },
@@ -41,7 +41,7 @@ export const dataProducts = [
     name: "Kaos Russ Straighfor SS Hitam",
     imageUrl:
       "https://www.russ.co.id/cdn/shop/products/BLACK_1000x.jpg?v=1739797254",
-    price: 104900,
+    price: 1049,
     stock: 13,
     description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
   },
@@ -51,7 +51,7 @@ export const dataProducts = [
     name: "Kaos Russ Truth White",
     imageUrl:
       "https://www.russ.co.id/cdn/shop/products/rug-1641875143251-2_1200x.jpg?v=1739797054",
-    price: 104900,
+    price: 1049,
     stock: 10,
     description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
   },
@@ -60,7 +60,7 @@ export const dataProducts = [
     name: "Russ Tshirt Kaos Pria Chikara White",
     imageUrl:
       "https://www.russ.co.id/cdn/shop/products/rug-1647523572131-1_1200x.jpg?v=1739796881",
-    price: 104900,
+    price: 1049,
     stock: 10,
     description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
   },
@@ -70,7 +70,7 @@ export const dataProducts = [
     name: "Russ Tshirt Kaos Pria Staycation White",
     imageUrl:
       "https://www.russ.co.id/cdn/shop/products/rug-1646629445015-6_1200x.jpg?v=1739796943",
-    price: 10900,
+    price: 1009,
     stock: 5,
     description: "Detail Produk : Bahan Combed 30,Kerah bulat,Sablon",
   },
